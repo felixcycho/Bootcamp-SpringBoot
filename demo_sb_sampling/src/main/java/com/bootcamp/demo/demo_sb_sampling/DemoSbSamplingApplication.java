18270ecca6b67121e42b6d284c8c58c67d846727
felixcycho <felixcycho@yahoo.com.hk>
2025-09-29 10:36:05 +0800

    felixcycho

---
+++ demo_sb_sampling/src/main/java/com/bootcamp/demo/demo_sb_sampling/DemoSbSamplingApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.demo.demo_sb_sampling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbSamplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSbSamplingApplication.class, args);
+	}
+
+}
